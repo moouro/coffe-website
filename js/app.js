f174fa4c33784f6078a6441d62381df7c0d011d5
Rodrigo Luiz <dolbrebr@gmail.com>
2021-09-17 16:03:29 -0300

    add home

--- js/app.js
+++ js/app.js
@@ -3,4 +3,17 @@ let navbar = document.querySelector('.navbar')
 
 menu.onclick = () => {
   menu.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
 }
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times')
+  navbar.classList.remove('active')
+}
+
+document.querySelectorAll('.image-slide img').forEach(images => {
+  images.onclick = () => {
+    let src = images.getAttribute('src')
+    document.querySelector('.main__home--image').src = src
+  }
+})
