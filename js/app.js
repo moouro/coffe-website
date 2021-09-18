e5cea1e1d54c73082e122b454eb3cd7d8625254a
Rodrigo Luiz <dolbrebr@gmail.com>
2021-09-18 11:06:38 -0300

    add lib slider

--- js/app.js
+++ js/app.js
@@ -17,3 +17,26 @@ document.querySelectorAll('.image-slide img').forEach(images => {
     document.querySelector('.main__home--image').src = src
   }
 })
+
+// plugin swiper
+var swiper = new Swiper('.review__slider', {
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    }
+  }
+})
